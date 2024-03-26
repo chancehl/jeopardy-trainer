a84c8621b649897c9b31ddc2076201bf96263e64
Chance Linz <clinz.dev@gmail.com>
2024-03-25 17:49:28 -0700

    adds web/ project with Vite + TS SPA

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,6 +8,14 @@ import (
 func main() {
 	router := gin.Default()
 
+	// static
+	router.Static("/assets", "./web/dist/assets")
+
+	// ui
+	router.GET("/", handler.ServeSPA)
+	router.NoRoute(handler.HandleSPARoute)
+
+	// games
 	router.GET("/games/:seed", handler.GetGame)
 	router.POST("/games", handler.CreateGame)
 

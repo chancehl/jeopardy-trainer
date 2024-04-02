2d5af9ab60af07dd10107ffd1593a666cef63119
Chance Linz <clinz.dev@gmail.com>
2024-04-01 21:31:11 -0700

    implemented the ability to skip the "npm run build" step on startup

--- internal/hooks/on_startup.go
+++ internal/hooks/on_startup.go
@@ -4,18 +4,39 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strconv"
 )
 
+func CheckIfShouldSkipBuild() bool {
+	skipNpmBuildEnvVariable := os.Getenv("SKIP_NPM_BUILD")
+
+	if skipNpmBuildEnvVariable == "" {
+		return false
+	}
+
+	shouldBuild, boolConversionErr := strconv.ParseBool(skipNpmBuildEnvVariable)
+
+	if boolConversionErr != nil {
+		log.Fatalf("Error parsing the boolean value: %s\n", boolConversionErr)
+	}
+
+	return shouldBuild
+}
+
 func OnServerStart() {
-	cmd := exec.Command("npm", "run", "build")
+	shouldSkipBuild := CheckIfShouldSkipBuild()
 
-	cmd.Dir = "./web"
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	if !shouldSkipBuild {
+		cmd := exec.Command("npm", "run", "build")
 
-	err := cmd.Run()
+		cmd.Dir = "./web"
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
 
-	if err != nil {
-		log.Fatalf("Failed to execute command: %s\n", err)
+		err := cmd.Run()
+
+		if err != nil {
+			log.Fatalf("Failed to execute command: %s\n", err)
+		}
 	}
 }
